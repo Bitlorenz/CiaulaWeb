cc6d6ed3f5b9e4187e4126a58213b4fd59e9987c
Bitlorenz <lollolucchi98@gmail.com>
2022-10-13 13:03:12 +0200

    fixed bug db, overriding save

--- HolidayPlanning/models.py
+++ HolidayPlanning/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from datetime import *
+'''classe modello per l'attrazione'''
 class Attrazione(models.Model):
     nome = models.CharField(max_length=200)
     posizione = models.CharField(max_length=300)
-    costo = models.DecimalField(max_digits=4, decimal_places=4, default=10.5)
-    durata = models.DurationField#questi sono i minuti
+    costo = models.FloatField(default=10.5)
+    durata = models.DurationField#questi sono i secondi della durata
     tipo = models.CharField(max_length=200)
     oraInizio = models.DateTimeField('ora inizio')
     oraFine = models.DateTimeField('ora fine')
 
+    #override del metodo save
+    def save(self, *args, **kwargs):
+        self.durata = self.oraFine - self.oraInizio
+        super(Attrazione, self).save(*args, **kwargs)#chiamata al vero metodo save
     #def __str__(self):
     #   return  self.__str__(self.nome, self.posizione, self.tipo)
-
-    def calcolaDurata(self):
-        return self.oraFine - self.oraInizio
 
 class Giornata(models.Model):
     numero = models.IntegerField#TODO chiave
