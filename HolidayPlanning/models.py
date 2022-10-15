ef4a5304c45686c0b8a6d0956e2b8fafb4d00abc
Bitlorenz <lollolucchi98@gmail.com>
2022-10-15 12:33:57 +0200

    updating HolidayPlanning models

--- HolidayPlanning/models.py
+++ HolidayPlanning/models.py
@@ -2,33 +2,43 @@ from django.db import models
 from datetime import *
 
 class Utente(models.Model):
-    nrSocio = models.IntegerField()#chiave
+    nrSocio = models.IntegerField(primary_key=True)#chiave
     nome = models.CharField()
     cognome = models.CharField()
-    codiceFiscale = models.CharField()
     email = models.CharField()
+    #codiceFiscale = models.CharField()
+    #telefono = models.CharField()
+    #dataDiNascita = models.DateField()
 
 '''classe modello per l'attrazione'''
 class Attrazione(models.Model):
-    nome = models.CharField(max_length=200)
-    posizione = models.CharField(max_length=300)
+    nome = models.CharField(max_length=200, primary_key=True)
+    luogo = models.CharField(max_length=300)
+    via = models.CharField(max_length=500)
+    citta = models.CharField(max_length=200)
     costo = models.FloatField(default=10.5)
-    durata = models.DurationField#questi sono i secondi della durata
     tipo = models.CharField(max_length=200)
-    oraInizio = models.DateTimeField('ora inizio')#TODO APERTURA
-    oraFine = models.DateTimeField('ora fine')#TODO CHIUSURA
-
-    #override del metodo save
-    def save(self, *args, **kwargs):
-        self.durata = self.oraFine - self.oraInizio
-        super(Attrazione, self).save(*args, **kwargs)#chiamata al vero metodo save
+    oraApertura = models.DateTimeField('ora apertura')
+    oraChiusura = models.DateTimeField('ora chiusura')
+    descrizione = models.TextField()
     #def __str__(self):
     #   return  self.__str__(self.nome, self.posizione, self.tipo)
 
 class Scelta(models.Model):
     #TODO chiave è composta dalla chiave di utente e di attrazione e giorno in mezzo
-    #TODO controllare che ora inizio e fine siano ammissibili
-    giorno = models.DateTimeField()
-    oraInizio = models.DateTimeField()
-    oraFine = models.DateTimeField()
-    numero = models.IntegerField()
+    giorno = models.DateTimeField() #scelto dall'utente
+    attrazione = models.ForeignKey('Attrazione')
+    oraInizio = models.DateTimeField(blank=True) #scelta dall'utente
+    oraFine = models.DateTimeField(blank=True) #scelta dall'utente
+    durata = models.DurationField  # questi sono i secondi della durata
+    posizioneInGiornata = models.IntegerField() #numero progressivo
+    #override del metodo save
+    def save(self, *args, **kwargs):
+        if(self.controllaOrari(self)):
+            self.durata = self.oraFine - self.oraInizio
+            # chiamata al vero metodo save
+            super(Attrazione, self).save(*args, **kwargs)
+
+    # TODO controllare che ora inizio e fine siano ammissibili
+    def controllaOrari(self):
+        return True
