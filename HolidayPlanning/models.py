dc6c77008e4b0ef44e0a704bdeeac62f068cd365
Bitlorenz <lollolucchi98@gmail.com>
2022-11-03 10:35:43 +0100

    finish load operations db

--- HolidayPlanning/models.py
+++ HolidayPlanning/models.py
@@ -1,21 +1,8 @@
 from django.db import models
-from datetime import *
 
 import profiles.models
-
-
-#TODO Utente dovrà essere spostato in un'altra sezione, destinata alla registrazione dell'account
-class Utente(models.Model):
-    nrSocio = models.IntegerField(primary_key=True)#chiave
-    nome = models.CharField()
-    cognome = models.CharField()
-    email = models.CharField()
-    #codiceFiscale = models.CharField()
-    #telefono = models.CharField()
-    #dataDiNascita = models.DateField()
-
-'''classe modello per l'attrazione'''
 class Attrazione(models.Model):
+    objects = None
     nome = models.CharField(max_length=200, primary_key=True)
     luogo = models.CharField(max_length=300)
     via = models.CharField(max_length=500)
@@ -25,33 +12,29 @@ class Attrazione(models.Model):
     oraApertura = models.DateTimeField('ora apertura')
     oraChiusura = models.DateTimeField('ora chiusura')
     descrizione = models.TextField()
-    #def __str__(self):
+    # def __str__(self):
     #   return  self.__str__(self.nome, self.posizione, self.tipo)
 
 class Scelta(models.Model):
-    giorno = models.DateTimeField() #scelto dall'utente
+    giorno = models.DateTimeField()  # scelto dall'utente
     attrazione = models.ForeignKey('Attrazione', on_delete=models.CASCADE)
-    utente = models.ForeignKey(profiles.models.UserProfileModel, related_name ='Utente', on_delete=models.CASCADE)
-    oraInizio = models.DateTimeField(blank=True) #scelta dall'utente
-    oraFine = models.DateTimeField(blank=True) #scelta dall'utente
+    utente = models.ForeignKey(profiles.models.UserProfileModel, related_name='Utente', on_delete=models.CASCADE)
+    oraInizio = models.DateTimeField(blank=True)  # scelta dall'utente
+    oraFine = models.DateTimeField(blank=True)  # scelta dall'utente
     durata = models.DurationField  # questi sono i secondi della durata
-    posizioneInGiornata = models.IntegerField() #numero progressivo
-    #override del metodo save
-    def save(self, *args, **kwargs):
-        if(self.controllaOrari(self)):
-            self.durata = self.oraFine - self.oraInizio
-            # chiamata al vero metodo save
-            super(Attrazione, self).save(*args, **kwargs)
+    posizioneInGiornata = models.IntegerField()  # numero progressivo
 
+    # override del metodo save
     # TODO controllare che ora inizio e fine siano ammissibili
-    def controllaOrari(self):
-        return True
+
 class Giornata(models.Model):
     data = models.DateField()
     numeroGiornata = models.IntegerField()
     totAttrazioni = models.IntegerField()
     totCosto = models.FloatField()
     scelte = models.ManyToManyField(Scelta, related_name='giornate')
+
+
 class Vacanza(models.Model):
     dataArrivo = models.DateTimeField()
     dataPartenza = models.DateTimeField()
--- ciaulaweb/initcmds.py
+++ ciaulaweb/initcmds.py
@@ -1,9 +1,5 @@
 # comandi iniziali
 from HolidayPlanning.models import Attrazione
-from django.utils import timezone
-from datetime import datetime, timedelta
-from threading import Timer
-
 
 def erase_db():
     print("Cancello il DB")
@@ -11,23 +7,27 @@ def erase_db():
 
 
 def init_db():
+    print("Controllo che non ci siano elementi...")
     if len(Attrazione.objects.all()) != 0:
         return
-
-    def func_time(off_year=None, off_month=None, off_day=None):
-        if off_year == 0 and off_month == 0 and off_day == 0:
-            return None
-        tz = timezone.now()
-        out = datetime(tz.year - off_year, tz.month - off_month,
-                       tz.day - off_day, tz.hour, tz.minute, tz.second)
-        return out
+    else:
+        print("Non ci sono elementi!")
 
     def processingAttrazioni():
         fileAttrazioni = open("entryAttrazioneDB.txt", "r")
+        listaNome = []
+        listaLuogo = []
+        listaCitta = []
+        listaVia = []
+        listaCosto = []
+        listaTipo = []
+        listaOraA = []
+        listaOraC = []
+        listaDescr = []
     # leggere il file con le attrazioni
         while True:
             line = fileAttrazioni.readline()
-            if line is "\n":
+            if line == "\n":
                 line = fileAttrazioni.readline()
             dataEntryList = line.rsplit(": ", 1)
             #if len(dataEntryList[1]) == 0 :
@@ -35,6 +35,57 @@ def init_db():
             # la riga non è una newline, possiamo salvare i campi nel dizionario attrazioni
             tipoCampo = dataEntryList[0]
             valoreCampo = dataEntryList[1]
-                # impostare uno switch
-                # aggiungo il valore di ogni campo ad una lista, c'è una lista per ogni campo
-                # creo un dizionario in cui aggiungo come key il tipo del campo e come value la rispettiva lista
+            # impostare uno switch, aggiungo il valore di ogni campo ad una lista, c'è una lista per ogni campo
+            if tipoCampo == "nome":
+                if valoreCampo == "":
+                    break #se il valore di nome non è stato inserito esco dal ciclo (attrazioni mancanti nel file)
+                else:
+                        print("Processo l'attrazione: "+valoreCampo)
+                listaNome.append(valoreCampo)
+            elif tipoCampo == "luogo":
+                listaLuogo.append(valoreCampo)
+            elif tipoCampo == "via":
+                listaVia.append(valoreCampo)
+            elif tipoCampo == "citta":
+                listaCitta.append(valoreCampo)
+            elif tipoCampo == "costo":
+                listaCosto.append(valoreCampo)
+            elif tipoCampo == "tipo":
+                listaTipo.append(valoreCampo)
+            elif tipoCampo == "oraApertura":
+                listaOraA.append(valoreCampo)
+            elif tipoCampo == "oraChiusura":
+                listaOraC.append(valoreCampo)
+            else: #tipoCampo e' descrizione
+                listaDescr.append(valoreCampo)
+        #inizializzo il dizionario
+        attrazioniDict = dict(nome=listaNome, luogo=listaLuogo, via=listaVia, citta=listaCitta, costo=listaCosto,
+                              tipo=listaTipo, oraApertura=listaOraA, oraChiusura=listaOraC, descrizione=listaDescr)
+        totEntries = len(listaNome)
+        for i in range(totEntries):
+            a = Attrazione()
+            for k in attrazioniDict:
+                if k == "nome":
+                        a.nome = attrazioniDict[k][i]
+                        print("Salvo attrazione "+a.nome+" nel db")
+                if k == "luogo":
+                        a.luogo = attrazioniDict[k][i]
+                if k == "via":
+                        a.via = attrazioniDict[k][i]
+                if k == "citta":
+                        a.citta = attrazioniDict[k][i]
+                if k == "costo":
+                        a.costo = attrazioniDict[k][i]
+                if k == "tipo":
+                        a.tipo = attrazioniDict[k][i]
+                if k == "oraApertura":
+                        a.oraApertura = attrazioniDict[k][i]
+                if k == "oraChiusura":
+                        a.oraChiusura = attrazioniDict[k][i]
+                else: #tipoCampo e' descrizione
+                        a.descrizione = attrazioniDict[k][i]
+            #print(a)
+            a.save()
+
+        print("DUMP DB")
+        print(Attrazione.objects.all())
