cf1569779aefb70dad36453441657fd3eeb32ae8
Bitlorenz <lollolucchi98@gmail.com>
2022-11-12 16:49:52 +0100

    aggiunta view scegliattr e scegliattr.html

--- HolidayPlanning/urls.py
+++ HolidayPlanning/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from django.contrib import admin
 from . import views
-from .views import lista_attrazioni
+from .views import lista_attrazioni, crea_scelta_da_citta
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("listaattrazioni/",lista_attrazioni, name="listaattrazioni")
+    path("listaattrazioni/",lista_attrazioni, name="listaattrazioni"),
+    path("scegliattr/", crea_scelta_da_citta, name="scegliattr")
 ]
