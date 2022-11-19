221f060c57deb6a58fbbb6554a52d3cc6b720b15
Bitlorenz <lollolucchi98@gmail.com>
2022-11-19 16:45:58 +0100

    modificaScelta view

--- HolidayPlanning/admin.py
+++ HolidayPlanning/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import Attrazione
+from django.contrib.auth.models import User, Group
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Only display the "username" field
+    fields = ["username"]
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 
 # Register your models here.
 admin.site.register(Attrazione)
+admin.site.register(User, UserAdmin)
