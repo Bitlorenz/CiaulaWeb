6149490755352d51503e1f2d7901fe15bc2b39b1
Bitlorenz <lorenzo@lucchina.it>
2023-02-22 16:12:49 +0100

    finita implementazione metodi crud

--- HolidayPlanning/urls.py
+++ HolidayPlanning/urls.py
@@ -6,8 +6,11 @@ app_name = 'HolidayPlanning'
 urlpatterns = [
     path("listaattrazioni/", lista_attrazioni, name="listaattrazioni"),
     path("scegliattr/", crea_scelta_da_citta, name="scegliattr"),
-    # es: /HolidayPlanning/cerca/PALERMO/
-    path("cerca/<str:attrazione_citta>/", attrazione, name='attrazione'),
     path("attrazioni/", AttrazioniList.as_view(), name="attrazioni"),
     path("scegliattrazione/", ScegliAttrazione.as_view(), name="scegliattrazione"),
+    path("listascelte/", ScelteList.as_view(), name="scelte"),
+    path("detscelta/<pk>/", DetailSceltaEntita.as_view(), name="dettaglioscelta"),
+    path("detattrazione/<pk>/", DetailAttrazioneEntita.as_view(), name="dettaglioattr"),
+    path("editscelta/<pk>/", ModificaScelta.as_view(), name="modificascelta"),
+    path("cancellascelta/<pk>", CancellaScelta.as_view(), name="cancellascelta"),
 ]
